021bb8617720368390da62a2d1a040dbf8f0acc1
Juan Pablo Fraga Terra <juanpablofragaterra@gmail.com>
2020-12-14 22:39:08 -0300

    Archivos Juan

---
+++ frase.js
@@ -0,0 +1,3 @@
+let frase = "El que va demasiado aprisa llega tan tarde como el que va muy despacio (Willian Shakespeare)"
+
+console.log(frase);
---
+++ multiplicacion.js
@@ -0,0 +1,3 @@
+function multiplicar(num1, num2) {
+    return num1 * num2;
+};
